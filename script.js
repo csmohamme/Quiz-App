1dd42fc9dc59ac983a0fcb532f0b9789b4cc4e92
csmohamme <cs.mohammedalmostafa@gmail.com>
2022-03-24 09:13:27 -0400

    create questions

---
+++ script.js
@@ -0,0 +1,34 @@
+const quizData = [
+  {
+    question: "Who created JavaScript ?",
+    a: "Mitchell Baker",
+    b: "Chris Beard",
+    c: "Brendan Eich",
+    d: "Yan Zhu",
+    correct: "c",
+  },
+  {
+    question: "What is the most used programming language in 2019?",
+    a: "Java",
+    b: "Python",
+    c: "C",
+    d: "JavaScript",
+    correct: "a",
+  },
+  {
+    question: "what does html stand for ?",
+    a: "Structured Query Language",
+    b: "Hypertext Markup Language",
+    c: "standard markup language",
+    d: "Cascading Style Sheets",
+    correct: "b",
+  },
+  {
+    question: "what year was javaScript launched ?",
+    a: "1999",
+    b: "2000",
+    c: "1995",
+    d: "1997",
+    correct: "c",
+  },
+];
