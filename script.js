1286177074e8913d635f2a7d8ca00339240ada43
csmohamme <cs.mohammedalmostafa@gmail.com>
2022-03-29 23:59:36 -0400

    creating functions[getSelected, deSelectAnswers] and showing score

--- script.js
+++ script.js
@@ -32,3 +32,59 @@ const quizData = [
     correct: "c",
   },
 ];
+const quizEl = document.getElementById("qiz");
+const answersEl = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_textEl = document.getElementById("a_text");
+const b_textEl = document.getElementById("b_text");
+const c_textEl = document.getElementById("c_text");
+const d_textEl = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+let currentQuiz = 0;
+let score = 0;
+
+// let answerEl = undefined;
+loadQuiz();
+
+function loadQuiz() {
+  deSelectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerHTML = currentQuizData.question;
+  a_textEl.innerHTML = currentQuizData.a;
+  b_textEl.innerHTML = currentQuizData.b;
+  c_textEl.innerHTML = currentQuizData.c;
+  d_textEl.innerHTML = currentQuizData.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+  answersEl.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+function deSelectAnswers() {
+  answersEl.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+submitBtn.addEventListener("click", () => {
+  //check to see the answer
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      // to show results
+      quizEl.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions</h2> <button onClick="location.reload()">Refresh</button>`;
+    }
+  }
+});
